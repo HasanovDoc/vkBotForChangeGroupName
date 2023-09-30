38e503f3ce590d6e8e14511891341710cd40ca90
HasanovDoc <hasanov326@mail.ru>
2023-09-30 12:14:29 +0400

    add change title on "Mag" group

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ weeNum = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=+4))).
 
 def changeTitleName(id, text):
     vk_session.method('messages.editChat', {'chat_id': id, 'title': text})
-    
-    
+
+
 def sendMessage(id, text):
     vk_session.method('messages.send', {'chat_id': id, 'random_id': 0, 'message': text})
 
@@ -25,15 +25,9 @@ def getTitleChatName(id):
         title = chat_settings['title']
     return title
 
-
-
-sendMessage(2, "Бот запущен")
-
-for event in longpoll.listen():
-    weeNum = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=+4))).isocalendar().week
-    id = event.chat_id
+def changeTitle(id):
     titleChat = getTitleChatName(id)
-    
+
     if datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=+4))).weekday() == WEEKDAYNUM:
         if weeNum % 2 == 0:
             if titleChat.count('ЗНАМЕНАТЕЛЬ') == 1:
@@ -41,59 +35,32 @@ for event in longpoll.listen():
         if weeNum % 2 == 1:
             if titleChat.count('ЧИСЛИТЕЛЬ') == 1:
                 changeTitleName(id, titleChat.replace('ЧИСЛИТЕЛЬ', 'ЗНАМЕНАТЕЛЬ'))
-                
+
+
+sendMessage(2, "Бот запущен")
+
+changeTitle(5)
+
+
+for event in longpoll.listen():
+    weeNum = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=+4))).isocalendar().week
+    id = event.chat_id
+    titleChat = getTitleChatName(id)
+
+    if datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=+4))).weekday() == WEEKDAYNUM:
+        if weeNum % 2 == 0:
+            if titleChat.count('ЗНАМЕНАТЕЛЬ') == 1:
+                changeTitleName(id, titleChat.replace('ЗНАМЕНАТЕЛЬ', 'ЧИСЛИТЕЛЬ'))
+        if weeNum % 2 == 1:
+            if titleChat.count('ЧИСЛИТЕЛЬ') == 1:
+                changeTitleName(id, titleChat.replace('ЧИСЛИТЕЛЬ', 'ЗНАМЕНАТЕЛЬ'))
+
 
     if event.type == VkBotEventType.MESSAGE_NEW:
         if event.from_chat:
             msg = event.object.message['text'].lower()
-                        
+
             if msg == '/bothello':
                 sendMessage(id, "Привет! Я бот, который меняет название беседы.")
-            
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# class MyLongPoll(VkBotLongPoll):
-#     def listen(self):
-#         while True:
-#             try:
-#                 for event in self.check():
-#                     yield event
-#             except Exception as e:
-#                 print(e)
-            
-# class VkBot:
-#     def __init__(self):
-#         self.vk_session = vk_api.VkApi(token = main_token)
-#         self.longpoll = MyLongPoll(self.vk_session, 222452856)
-#         # self.vk_id = self.vk_session.get_user_id()
-#         # self.vk_name = self.vk_session.get_user_name()
-#         # self.vk_photo = self.vk_session.get_user_photo(self.vk_id)
-#         # self.vk_friends = self.vk_session.friends.get()
-#         # self.vk_groups = self.vk_session.groups.get()
-#         # self.vk_wall = self.vk_session.wall.get(owner_id=self.vk_id)
-#         # self.vk_messages = self.vk_session.messages.get(owner_id=self.vk_id)
-#     def run(self):
-#         for event in self.longpoll.listen():
-#             if event.type == VkEventType.MESSAGE_NEW:
-#                 msg = event.object.message
-        
-# if __name__ == '__main__':
-#     VkBot().run()
-        
-
-# vk_session = vk_api.VkApi(token=main_token)
-# longpoll = VkLongPoll(vk_session)
-
+            if msg == '/getid':
+                print(f"id бесседы - {id}")
